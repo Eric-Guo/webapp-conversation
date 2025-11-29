4dafbfb5ff589c1ee333c710b9631b3266738520
Eric-Guo <eric.guocz@gmail.com>
2025-11-30 00:18:08 +0800

    ✨Change const userPrefix = `user_${APP_ID}:` into user_${session?.user?.name}

--- app/api/utils/common.ts
+++ app/api/utils/common.ts
@@ -1,11 +1,12 @@
 import type { NextRequest } from 'next/server'
 import { ChatClient } from 'dify-client'
 import { v4 } from 'uuid'
-import { API_KEY, API_URL, APP_ID, APP_INFO } from '@/config'
+import { auth } from '@/auth'
+import { APP_ID, API_KEY, API_URL, APP_INFO } from '@/config'
 
-const userPrefix = `user_${APP_ID}:`
-
-export const getInfo = (request: NextRequest) => {
+export const getInfo = async (request: NextRequest) => {
+  const session = await auth()
+  const userPrefix = `user_${APP_ID}_${session?.user?.name}:`
   const sessionId = request.cookies.get('session_id')?.value || v4()
   const user = userPrefix + sessionId
   return {
